e4254891863c864cfee1c9fbf456cff2f60bb0f1
nipun1a <nipun1a1b@gmail.com>
2026-05-23 12:32:38 +0530

    database is succesfully connected to the backend

--- Arya/server/server.ts
+++ Arya/server/server.ts
@@ -1,8 +1,23 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+
 
 const app = express();
 const port = 3000;
+
+const corsOptions = {
+    origin: process.env.TRUSTED_ORIGINS?.split(','),
+    Credentials: true,
+    optionsSuccessStatus: 200,
+}
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World! from the server');
